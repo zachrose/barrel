72367ee18b731b94560f06ff057ddf200e3b9a96
Zach Rose <zach@zachrose.biz>
2014-07-11 01:32:36 -0700

    Refactor

--- player.js
+++ player.js
@@ -2,6 +2,7 @@ var Player = function(doer){
   var self = this;
   this.doer = doer || function(){};
   this._playHead = 0;
+  this._roller = null;
 }
 
 Player.prototype.load = function(track){
@@ -10,36 +11,52 @@ Player.prototype.load = function(track){
 }
 
 Player.prototype.play = function(){
-  var self = this;
-  if(!this.track){
-    throw "Player has not track to play";
-  }
+  if(!this.track){ throw "Player has not track to play"; }
+  this._schedule();
+  this._startRoller();
+  return this;
+}
+
+Player.prototype.stop = function(){
+  this.pause().rewind();;
+  return this;
+}
+
+Player.prototype.pause = function(){
+  this._stopRoller();
+  this._cancelScheduled();
+  return this;
+}
+
+Player.prototype.rewind = function(){
+  this._playHead = 0;
+  return this;
+}
+
+Player.prototype._startRoller = function(){
+  self = this;
+  this._roller = setInterval(function(){
+    self._playHead += 1;
+  }, 1);
+}
+
+Player.prototype._stopRoller = function(){
+  clearInterval(this._roller);
+}
+
+Player.prototype._schedule = function(){
+  self = this;
   this._timeouts = this.track.filter(function(event){
     return event.t >= self._playHead;
   }).map(function(event){
     return setTimeout(self.doer, event.t - self._playHead, event.d);
   });
-  this._roller = setInterval(function(){
-    self._playHead += 1;
-  }, 1);
-  return this;
 }
 
-Player.prototype.stop = function(){
+Player.prototype._cancelScheduled = function(){
   this._timeouts.forEach(function(timeout){
     clearTimeout(timeout);
   });
-  clearInterval(this._roller);
-  this._playHead = 0;
-  return this;
-}
-
-Player.prototype.pause = function(){
-  this._timeouts.forEach(function(timeout){
-    clearTimeout(timeout);
-  });
-  clearInterval(this._roller);
-  return this;
 }
 
 module.exports = Player;
