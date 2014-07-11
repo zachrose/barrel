9483adb0c33f2c8e1e85d91cd77470268f98dd9e
Zach Rose <zach@zachrose.biz>
2014-07-11 03:09:29 -0700

    Cosmetics

--- demos/web/socket_player.js
+++ demos/web/socket_player.js
@@ -4,22 +4,21 @@ var WebSocketServer = require('ws').Server,
     Player = require('../../player');
 
 var track = [
-  { t: 200*1,  d: {hue:   0, saturation: 100, lightness: 50}},
-  { t: 200*2,  d: {hue:  60, saturation: 100, lightness: 50}},
-  { t: 200*3,  d: {hue: 120, saturation: 100, lightness: 50}},
-  { t: 200*4,  d: {hue: 180, saturation: 100, lightness: 50}},
-  { t: 200*5,  d: {hue: 240, saturation: 100, lightness: 50}},
-  { t: 200*6,  d: {hue: 300, saturation: 100, lightness: 50}},
+  { t: 100,  d: {hue:   0, saturation: 100, lightness: 50}},
+  { t: 200,  d: {hue:  60, saturation: 100, lightness: 50}},
+  { t: 300,  d: {hue: 120, saturation: 100, lightness: 50}},
+  { t: 400,  d: {hue: 180, saturation: 100, lightness: 50}},
+  { t: 500, d: {hue: 240, saturation: 100, lightness: 50}},
+  { t: 600, d: {hue: 300, saturation: 100, lightness: 50}},
 ];
 
-wss = new WebSocketServer({port: 8080})
+webSocketServer = new WebSocketServer({port: 8080})
 
-wss.on('connection', function(ws){
-    ws.on('message', function(message){
-        player = new Player(function(d){
-          ws.send(JSON.stringify(d));
-        }).load(track2).play();
-    });
+webSocketServer.on('connection', function(socket){
+  socket.on('message', function(message){
+    var sender = function(d){ socket.send(JSON.stringify(d)); };
+    var player = new Player(sender).load(track).play();
+  });
 });
  
 app.get('/', function(req, res){
