4f2d12429be848101c81097f16b3f436466d473d
Zach Rose <zach@zachrose.biz>
2014-07-14 22:36:56 -0700

    Rename nested tracks to 'channels'

--- test/muxer.js
+++ test/muxer.js
@@ -3,33 +3,37 @@ var should = require('should'),
 
 describe("Muxer", function(){
 
-  it("it can mux two named tracks into one", function(){
+  it("it can mux many named tracks into one track with many channels", function(){
       var muxed = new Muxer().mux({
         'count': [{t: 0, d: 'one'},{t: 10, d: 'two'}],
-        'alphabet': [{t: 0, d: 'A'},{t: 10, d: 'B'},{t: 20, d: 'C'}]
+        'alphabet': [{t: 0, d: 'A'},{t: 10, d: 'B'},{t: 20, d: 'C'}],
+        'numbers': [{t: 25, d: 3}]
       });
       var expected = [
-        {t: 0,  track: 'count',    d: 'one'},
-        {t: 0,  track: 'alphabet', d: 'A'},
-        {t: 10, track: 'count',    d: 'two'},
-        {t: 10, track: 'alphabet', d: 'B'},
-        {t: 20, track: 'alphabet', d: 'C'}
+        {t: 0,  channel: 'count',    d: 'one'},
+        {t: 0,  channel: 'alphabet', d: 'A'},
+        {t: 10, channel: 'count',    d: 'two'},
+        {t: 10, channel: 'alphabet', d: 'B'},
+        {t: 20, channel: 'alphabet', d: 'C'},
+        {t: 25, channel: 'numbers', d: 3}
       ];
       muxed.forEach(function(event){ expected.should.containEql(event); });
       expected.forEach(function(event){ muxed.should.containEql(event); });
   });
   
-  it("can demux previously muxed named tracks", function(){
+  it("can demux one track with many channels into many named tracks", function(){
     var muxed = [
-      {t: 0,  track: 'count',    d: 'one'},
-      {t: 0,  track: 'alphabet', d: 'A'},
-      {t: 10, track: 'count',    d: 'two'},
-      {t: 10, track: 'alphabet', d: 'B'},
-      {t: 20, track: 'alphabet', d: 'C'}
+      {t: 0,  channel: 'count',    d: 'one'},
+      {t: 0,  channel: 'alphabet', d: 'A'},
+      {t: 10, channel: 'count',    d: 'two'},
+      {t: 10, channel: 'alphabet', d: 'B'},
+      {t: 20, channel: 'alphabet', d: 'C'},
+      {t: 25, channel: 'numbers', d: 3}
     ];
     var expected = {
       'count': [{t: 0, d: 'one'},{t: 10, d: 'two'}],
-      'alphabet': [{t: 0, d: 'A'},{t: 10, d: 'B'},{t: 20, d: 'C'}]
+      'alphabet': [{t: 0, d: 'A'},{t: 10, d: 'B'},{t: 20, d: 'C'}],
+      'numbers': [{t: 25, d: 3}]
     }
     var actual = new Muxer().demux(muxed);
     should(actual).eql(expected);
