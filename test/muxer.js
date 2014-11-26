7ffa4f6968fe0315f40cccafd517518aadd2886c
Zach Rose <zach@zachrose.biz>
2014-11-25 21:25:46 -0800

    Update muxer test to get with event.data & event.time

--- test/muxer.js
+++ test/muxer.js
@@ -5,17 +5,17 @@ describe("Muxer", function(){
 
   it("it can mux many named tracks into one track with many channels", function(){
       var muxed = muxer.mux({
-        'count': [{t: 0, d: 'one'},{t: 10, d: 'two'}],
-        'alphabet': [{t: 0, d: 'A'},{t: 10, d: 'B'},{t: 20, d: 'C'}],
-        'numbers': [{t: 25, d: 3}]
+        'count': [{time: 0, data: 'one'},{time: 10, data: 'two'}],
+        'alphabet': [{time: 0, data: 'A'},{time: 10, data: 'B'},{time: 20, data: 'C'}],
+        'numbers': [{time: 25, data: 3}]
       });
       var expected = [
-        {t: 0,  channel: 'count',    d: 'one'},
-        {t: 0,  channel: 'alphabet', d: 'A'},
-        {t: 10, channel: 'count',    d: 'two'},
-        {t: 10, channel: 'alphabet', d: 'B'},
-        {t: 20, channel: 'alphabet', d: 'C'},
-        {t: 25, channel: 'numbers', d: 3}
+        {time: 0,  channel: 'count',    data: 'one'},
+        {time: 0,  channel: 'alphabet', data: 'A'},
+        {time: 10, channel: 'count',    data: 'two'},
+        {time: 10, channel: 'alphabet', data: 'B'},
+        {time: 20, channel: 'alphabet', data: 'C'},
+        {time: 25, channel: 'numbers', data: 3}
       ];
       muxed.forEach(function(event){ expected.should.containEql(event); });
       expected.forEach(function(event){ muxed.should.containEql(event); });
@@ -23,17 +23,17 @@ describe("Muxer", function(){
   
   it("can demux one track with many channels into many named tracks", function(){
     var muxed = [
-      {t: 0,  channel: 'count',    d: 'one'},
-      {t: 0,  channel: 'alphabet', d: 'A'},
-      {t: 10, channel: 'count',    d: 'two'},
-      {t: 10, channel: 'alphabet', d: 'B'},
-      {t: 20, channel: 'alphabet', d: 'C'},
-      {t: 25, channel: 'numbers', d: 3}
+      {time: 0,  channel: 'count',    data: 'one'},
+      {time: 0,  channel: 'alphabet', data: 'A'},
+      {time: 10, channel: 'count',    data: 'two'},
+      {time: 10, channel: 'alphabet', data: 'B'},
+      {time: 20, channel: 'alphabet', data: 'C'},
+      {time: 25, channel: 'numbers', data: 3}
     ];
     var expected = {
-      'count': [{t: 0, d: 'one'},{t: 10, d: 'two'}],
-      'alphabet': [{t: 0, d: 'A'},{t: 10, d: 'B'},{t: 20, d: 'C'}],
-      'numbers': [{t: 25, d: 3}]
+      'count': [{time: 0, data: 'one'},{time: 10, data: 'two'}],
+      'alphabet': [{time: 0, data: 'A'},{time: 10, data: 'B'},{time: 20, data: 'C'}],
+      'numbers': [{time: 25, data: 3}]
     }
     var actual = muxer.demux(muxed);
     should(actual).eql(expected);
