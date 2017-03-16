d4ccae45129efea68a9ccaaabe03d4fedcc317c6
Zach Rose <zach@zachrose.biz>
2017-03-16 03:19:22 -0700

    Polish CLI demo

--- demos/cli.js
+++ demos/cli.js
@@ -2,24 +2,28 @@
 
 var Player = require('../lib/player');
 
-new Player(function(d){
-  console.log(d);
-}).load([
-  { time: 1.2*0,     data: 'Come' },
-  { time: 1.2*500,   data: 'as' },
-  { time: 1.2*750,   data: 'you' },
-  { time: 1.2*875,   data: 'are' },
-  { time: 1.2*1500,  data: 'as' },
-  { time: 1.2*1750,  data: 'you' },
-  { time: 1.2*1825,  data: 'were' },
-  { time: 1.2*2675,  data: 'as' },
-  { time: 1.2*2750,  data: 'I' },
-  { time: 1.2*2850,  data: 'want' },
-  { time: 1.2*3000,  data: 'you' },
-  { time: 1.2*3250,  data: 'to' },
-  { time: 1.2*3500,  data: 'be' },
-]).play();
+var nirvana = [
+  { time: 0,    data: 'come' },
+  { time: 975,  data: 'as' },
+  { time: 1325, data: 'you' },
+  { time: 1675, data: 'are' },
+  { time: 2700, data: 'as' },
+  { time: 3150, data: 'you' },
+  { time: 3450, data: 'were' },
+  { time: 4463, data: 'as' },
+  { time: 4805, data: 'I' },
+  { time: 5180, data: 'want' },
+  { time: 5785, data: 'you' },
+  { time: 6580, data: 'to' },
+  { time: 6830, data: 'be' },
+  { time: 7580, data: 0 }
+];
 
-setTimeout(function(){
-  process.exit(0);
-}, 1.2*4000);
+var doer = function(e){
+  return {
+    string: (s) => console.log(s),
+    number: (n) => process.exit(n)
+  }[typeof e](e);
+};
+
+new Player(doer).load(nirvana).play();
