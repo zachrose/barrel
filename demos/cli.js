adacfff1d7c5bbcceb34b78f575bbae80e6f0be2
Zach Rose <zach@zachrose.biz>
2017-03-16 00:15:46 -0700

    Include demos, tests, etc in linting

--- demos/cli.js
+++ demos/cli.js
@@ -1,21 +1,23 @@
-Player = require('../lib/player');
+'use strict';
 
-player = new Player(function(d){
+var Player = require('../lib/player');
+
+new Player(function(d){
   console.log(d);
 }).load([
-  { time: 1.2*0,     data: "Come" },
-  { time: 1.2*500,   data: "as" },
-  { time: 1.2*750,   data: "you" },
-  { time: 1.2*875,   data: "are" },
-  { time: 1.2*1500,  data: "as" },
-  { time: 1.2*1750,  data: "you" },
-  { time: 1.2*1825,  data: "were" },
-  { time: 1.2*2675,  data: "as" },
-  { time: 1.2*2750,  data: "I" },
-  { time: 1.2*2850,  data: "want" },
-  { time: 1.2*3000,  data: "you" },
-  { time: 1.2*3250,  data: "to" },
-  { time: 1.2*3500,  data: "be" },
+  { time: 1.2*0,     data: 'Come' },
+  { time: 1.2*500,   data: 'as' },
+  { time: 1.2*750,   data: 'you' },
+  { time: 1.2*875,   data: 'are' },
+  { time: 1.2*1500,  data: 'as' },
+  { time: 1.2*1750,  data: 'you' },
+  { time: 1.2*1825,  data: 'were' },
+  { time: 1.2*2675,  data: 'as' },
+  { time: 1.2*2750,  data: 'I' },
+  { time: 1.2*2850,  data: 'want' },
+  { time: 1.2*3000,  data: 'you' },
+  { time: 1.2*3250,  data: 'to' },
+  { time: 1.2*3500,  data: 'be' },
 ]).play();
 
 setTimeout(function(){
